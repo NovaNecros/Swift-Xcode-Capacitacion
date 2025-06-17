8f4cb1c014272d2f0e115587c8328487187ce6cf
NovaNecros <uchihaespurr@gmail.com>
2025-06-17 10:52:02 -0600

    Initial Commit

---
+++ Hola Mundo App/Hola_Mundo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hola_Mundo_AppApp.swift
+//  Hola Mundo App
+//
+//  Created by Desarrollo Finantech on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Hola_Mundo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hola Mundo AppTests/Hola_Mundo_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hola_Mundo_AppTests.swift
+//  Hola Mundo AppTests
+//
+//  Created by Desarrollo Finantech on 17/06/25.
+//
+
+import Testing
+@testable import Hola_Mundo_App
+
+struct Hola_Mundo_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
