90bc5070cc175ffdcc78c2c5588fdf489d03b814
NovaNecros <uchihaespurr@gmail.com>
2025-06-17 11:29:19 -0600

    Mi primera aplicación de iOS.

--- Hola Mundo App/ContentView.swift
+++ Hola Mundo App/ContentView.swift
@@ -1,24 +1,52 @@
-//
-//  ContentView.swift
-//  Hola Mundo App
-//
-//  Created by Desarrollo Finantech on 17/06/25.
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ContentView : View
+{
+    @State private var mostrar : Bool = false
+    
+    var body : some View
+    {
+        VStack(spacing: 20)
+        {
+            if mostrar
+            {
+                Image(systemName: "heart.fill")
+                    .font(.system(size: 100))
+                    .foregroundColor(.black)
+                    .transition(.scale)
+                    .padding(.top, 10)
+                
+                Text("Hola Mundo <3")
+                    .font(.largeTitle)
+                    .foregroundColor(.pink)
+                    .padding()
+            }
+
+            Button(action:
+            {
+                withAnimation(.spring())
+                {
+                    mostrar = !mostrar
+                }
+            })
+            {
+                Text("Apachurráme")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding(.vertical, 12)
+                    .padding(.horizontal, 24)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .fill(Color.pink.opacity(0.8)))
+                    .shadow(color: Color.pink.opacity(0.4), radius: 8, x: 0, y: 4)
+            }
+            .buttonStyle(PlainButtonStyle())
         }
         .padding()
     }
 }
 
-#Preview {
+#Preview
+{
     ContentView()
 }
