e1e9e89bcc4c4af0077341334ef403e5fb7e8091
NovaNecros <uchihaespurr@gmail.com>
2025-06-20 12:52:39 -0600

    Initial Commit

---
+++ Personitas AppTests/Personitas_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Personitas_AppTests.swift
+//  Personitas AppTests
+//
+//  Created by Desarrollo Finantech on 20/06/25.
+//
+
+import Testing
+@testable import Personitas_App
+
+struct Personitas_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
