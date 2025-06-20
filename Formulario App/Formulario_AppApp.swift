5730216744413b0ab24728959af2d1c68398163c
NovaNecros <uchihaespurr@gmail.com>
2025-06-20 13:58:11 -0600

    Initial Commit

---
+++ Formulario App/Formulario_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Formulario_AppApp.swift
+//  Formulario App
+//
+//  Created by Desarrollo Finantech on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Formulario_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
