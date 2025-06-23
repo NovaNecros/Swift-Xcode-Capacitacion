e15636d64d81dd504cd31477853c979724ca2a45
NovaNecros <uchihaespurr@gmail.com>
2025-06-23 15:50:51 -0600

    Initial Commit

---
+++ Audio App/Audio_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_AppApp.swift
+//  Audio App
+//
+//  Created by Desarrollo Finantech on 23/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Audio_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Audio AppTests/Audio_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_AppTests.swift
+//  Audio AppTests
+//
+//  Created by Desarrollo Finantech on 23/06/25.
+//
+
+import Testing
+@testable import Audio_App
+
+struct Audio_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
