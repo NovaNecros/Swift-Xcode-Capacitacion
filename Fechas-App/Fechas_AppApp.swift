83b5184f1b0ed1e1f65841ed8ccfe1620a62c1e5
NovaNecros <uchihaespurr@gmail.com>
2025-06-23 14:47:15 -0600

    Initial Commit

---
+++ Fechas-App/Fechas_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fechas_AppApp.swift
+//  Fechas-App
+//
+//  Created by Desarrollo Finantech on 23/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Fechas_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fechas-AppTests/Fechas_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fechas_AppTests.swift
+//  Fechas-AppTests
+//
+//  Created by Desarrollo Finantech on 23/06/25.
+//
+
+import Testing
+@testable import Fechas_App
+
+struct Fechas_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
