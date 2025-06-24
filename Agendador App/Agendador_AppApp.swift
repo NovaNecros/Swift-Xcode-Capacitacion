dd0b19189ad6741d348b57d5934f8e13dffb49e9
NovaNecros <uchihaespurr@gmail.com>
2025-06-24 13:22:31 -0600

    Initial Commit

---
+++ Agendador App/Agendador_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Agendador_AppApp.swift
+//  Agendador App
+//
+//  Created by Desarrollo Finantech on 24/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Agendador_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Agendador AppTests/Agendador_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Agendador_AppTests.swift
+//  Agendador AppTests
+//
+//  Created by Desarrollo Finantech on 24/06/25.
+//
+
+import Testing
+@testable import Agendador_App
+
+struct Agendador_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
