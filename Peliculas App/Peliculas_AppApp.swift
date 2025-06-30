ec02129113085cc84df7aa827782f9e74b3334ca
NovaNecros <uchihaespurr@gmail.com>
2025-06-30 13:21:17 -0600

    Initial Commit

---
+++ Peliculas App/Peliculas_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Peliculas_AppApp.swift
+//  Peliculas App
+//
+//  Created by Desarrollo Finantech on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Peliculas_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Peliculas AppTests/Peliculas_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Peliculas_AppTests.swift
+//  Peliculas AppTests
+//
+//  Created by Desarrollo Finantech on 30/06/25.
+//
+
+import Testing
+@testable import Peliculas_App
+
+struct Peliculas_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
